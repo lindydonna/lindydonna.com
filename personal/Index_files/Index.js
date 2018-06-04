91c2b86fbb869531070a94f455a3c71723f6a17b
Donna Malayeri <lindydonna@users.noreply.github.com>
2018-06-03 22:59:05 -0700

    Updates to CSS and content

--- personal/Index_files/Index.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20080922
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Index_files/IndexMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');performPostEffectsFixups()}
